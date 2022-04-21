8df9528f09c79dc416d0b94fd3bd19b4457074ca
eduardoperetto <erperetto@gmail.com>
2022-04-20 21:36:35 -0300

    Add models and print functions

---
+++ features.py
@@ -0,0 +1,14 @@
+from models import Playlist
+
+class PlaylistFeatures(Playlist):
+    def sort_by_most_listened_by_artist():
+        print("WIP")
+
+    def sort_by_most_language():
+        print("WIP")
+
+    def sort_by_most_weekly_most_listened():
+        print("WIP")
+
+    def sort_by_release_date():
+        print("WIP")
---
+++ models.py
@@ -0,0 +1,32 @@
+
+from distutils.log import error
+
+
+class Track:
+    def __init__(self, _name, _artist, _popularity, _releaseDate):
+        self.name = _name
+        self.artist = _artist
+        # self.language = _language
+        # self.listeningFrequency = _listeningFrequency
+        self.popularity = _popularity
+        self.releaseDate = _releaseDate
+
+class Playlist:
+    def __init__(self, _tracks):
+        self.tracks = _tracks
+
+    def addTrack(self, _track):
+        self.tracks.append(_track)
+    
+    def printTracksDetail(self):
+        for track, i in zip(self.tracks, range(len(self.tracks))):
+            print(f'Track #{i} -> {track.name} from {track.artist}')
+            try:
+                print(f'     --> Popularity: {track.popularity}')
+                print(f'     --> Release date: {track.releaseDate}')
+            except error:
+                print(error)
+
+    def printTracksOrder(self):
+        for track, i in zip(self.tracks, range(len(self.tracks))):
+            print(f'#{i}: {track.name}')
