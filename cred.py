873ef410b26f29b7d6a51703cd67f83bbff3ea85
Eduardo Marques <marquesdu2009@hotmail.com>
2022-09-25 12:13:22 -0300

    Update cred.py

--- cred.py
+++ cred.py
@@ -1,3 +1,3 @@
-client_ID='06633054be5c4d449774bcad92838a67'
-client_SECRET='cd0b05b4cbc64f38a686be1ff6c37a82'
-redirect_url='http://localhost:9000'
+client_ID='your-id-here'
+client_SECRET='your-secret-here'
+redirect_url='http://localhost:port'
